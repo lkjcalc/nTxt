025dc3bd2f03e96c76a76b848eaac89e5f54a002
lkj <lkjcalc@gmail.com>
2017-03-14 08:53:38 +0100

    fix custom syscall address order

--- clipboard.c
+++ clipboard.c
@@ -8,12 +8,13 @@ static const unsigned clipboard_get_addrs[] = {0x104A1B94, 0x104860A8, //Classic
                                                0x1058ABCC, 0x1056F3F8, //Classic 3.6
                                                0x1056C814, 0x1056D2AC, //CX 3.6
                                                0x10591E04, 0x10576130, //Classic 3.9.0
-                                               0, 0, //Classic 3.9.1
                                                0x1057395C, 0x10573E64, //CX 3.9.0
+                                               0, 0, //Classic 3.9.1
                                                0, 0, //CX 3.9.1
                                                0, 0, //CX 4.0.0
                                                0x10581C28, 0x10582194, //CX 4.0.3
                                                0x10596AF0, 0x1059709C, //CX 4.2
+                                               0, 0, //CX 4.3
                                                0x105A73E4, 0x105A7988 //CX 4.4
                                               };
 #define os_clipboard_get SYSCALL_CUSTOM(clipboard_get_addrs, int, char**, const char*)
@@ -25,12 +26,13 @@ static const unsigned clipboard_add_addrs[] = {0x104A1E38, 0x1048634C, //Classic
                                                0x1058AE58, 0x1056F684, //Classic 3.6
                                                0x1056CAA0, 0x1056D538, //CX 3.6
                                                0x10592080, 0x105763AC, //Classic 3.9.0
-                                               0, 0, //Classic 3.9.1
                                                0x10573BD8, 0x105740E0, //CX 3.9.0
+                                               0, 0, //Classic 3.9.1
                                                0, 0, //CX 3.9.1
                                                0, 0, //CX 4.0.0
                                                0x10581EA4, 0x10582410, //CX 4.0.3
                                                0x10596D6C, 0x10597318, //CX 4.2
+                                               0, 0, //CX 4.3
                                                0x105A7660, 0x105A7C04 //CX 4.4
                                               };
 #define os_clipboard_add SYSCALL_CUSTOM(clipboard_add_addrs, int, const char*, int, const char*)
@@ -42,12 +44,13 @@ static const unsigned clipboard_reset_addrs[] = {0x104A1EE4, 0x104863F8, //Class
                                                  0x1058AF34, 0x1056F760, //Classic 3.6
                                                  0x1056CB7C, 0x1056D614, //CX 3.6
                                                  0x1059215C, 0x10576488, //Classic 3.9.0
-                                                 0, 0, //Classic 3.9.1
                                                  0x10573CB4, 0x105741BC, //CX 3.9.0
+                                                 0, 0, //Classic 3.9.1
                                                  0, 0, //CX 3.9.1
                                                  0, 0, //CX 4.0.0
                                                  0x10581F80, 0x105824EC, //CX 4.0.3
                                                  0x10596E48, 0x105973F4, //CX 4.2
+                                                 0, 0, //CX 4.3
                                                  0x105A773C, 0x105A7CE0 //CX 4.4
                                                 };
 #define os_clipboard_reset SYSCALL_CUSTOM(clipboard_reset_addrs, int, void)
